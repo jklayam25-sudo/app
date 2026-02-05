e77d8a6f2fee47de3d5aecc0c3439e33874d0845
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-06 06:47:17 +0700

    fix(trx): fixed interface repo PK identifier Long to UUID

--- src/main/java/lumi/insert/app/repository/TransactionRepository.java
+++ src/main/java/lumi/insert/app/repository/TransactionRepository.java
@@ -1,14 +1,16 @@
 package lumi.insert.app.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import lumi.insert.app.entity.Transaction;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+public interface TransactionRepository extends JpaRepository<Transaction, UUID>, JpaSpecificationExecutor<Transaction>{
     
     Optional<Transaction> findByInvoiceId(String invoiceId);
 
