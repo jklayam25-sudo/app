80f0eecbd70a899f9f2d1745087007a86efc2a05
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-09 03:33:17 +0700

    feat(allTrx): create mapper for all Transaction entity to DTO

--- src/main/java/lumi/insert/app/utils/mapper/AllTransactionMapper.java
+++ src/main/java/lumi/insert/app/utils/mapper/AllTransactionMapper.java
@@ -2,15 +2,33 @@ package lumi.insert.app.utils.mapper;
 
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-
+ 
+import lumi.insert.app.dto.response.TransactionItemDelete;
+import lumi.insert.app.dto.response.TransactionItemResponse;
+import lumi.insert.app.dto.response.TransactionPaymentResponse;
 import lumi.insert.app.dto.response.TransactionResponse;
 import lumi.insert.app.entity.Transaction;
+import lumi.insert.app.entity.TransactionItem;
+import lumi.insert.app.entity.TransactionPayment;
 
 @Mapper(componentModel = "spring")
 public interface AllTransactionMapper {
     
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
     TransactionResponse createTransactionResponseDto(Transaction transaction);
+
+    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(target = "productId", source = "product.id")
+    TransactionItemResponse createTransactionItemResponseDto(TransactionItem transactionItem);
+
+    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(target = "deleted", constant = "true")
+    TransactionItemDelete createTransactionItemDeleteResponseDto(TransactionItem transactionItem);
+
+    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(target = "transactionId", source = "transaction.id")
+    TransactionPaymentResponse createTransactionPaymentResponseDto(TransactionPayment transactionPayment);
 }
