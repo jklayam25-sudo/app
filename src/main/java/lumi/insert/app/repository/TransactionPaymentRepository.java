b57d1cab1d93217a93990b4de7318e1622e7fe77
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-09 02:53:48 +0700

    fix & feat(trxPayment): fix interface repo PK identifier Long to UUID  and add JpaSpecExecuter

--- src/main/java/lumi/insert/app/repository/TransactionPaymentRepository.java
+++ src/main/java/lumi/insert/app/repository/TransactionPaymentRepository.java
@@ -5,12 +5,13 @@ import java.util.UUID;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
  
 import lumi.insert.app.entity.TransactionPayment;
 
 @Repository
-public interface TransactionPaymentRepository extends  JpaRepository<TransactionPayment, Long>{
+public interface TransactionPaymentRepository extends  JpaRepository<TransactionPayment, UUID>, JpaSpecificationExecutor<TransactionPayment>{
     
     Slice<TransactionPayment> findAllByTransactionId(UUID transactionId, Pageable pageable);
 
