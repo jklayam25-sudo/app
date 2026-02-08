be396d563ffee4d65a15f60a995b625cee472528
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-09 03:31:03 +0700

    refactor(trxItem & trxPayment): change the return of method

--- src/main/java/lumi/insert/app/service/TransactionItemService.java
+++ src/main/java/lumi/insert/app/service/TransactionItemService.java
@@ -7,17 +7,17 @@ import org.springframework.data.domain.Slice;
 import lumi.insert.app.dto.request.PaginationRequest;
 import lumi.insert.app.dto.request.TransactionItemCreateRequest;
 import lumi.insert.app.dto.response.TransactionItemDelete;
-import lumi.insert.app.entity.TransactionItem;
+import lumi.insert.app.dto.response.TransactionItemResponse; 
 
 public interface TransactionItemService {
     
-    TransactionItem createTransactionItem(UUID transactionId, TransactionItemCreateRequest request);
+    TransactionItemResponse createTransactionItem(UUID transactionId, TransactionItemCreateRequest request);
 
     TransactionItemDelete deleteTransactionItem(UUID id);
 
-    TransactionItem updateTransactionItemQuantity(UUID id, Long quantity);
+    TransactionItemResponse updateTransactionItemQuantity(UUID id, Long quantity);
 
-    Slice<TransactionItem> getTransactionItemsByTransactionId(UUID transactionId, PaginationRequest paginationRequest);
+    Slice<TransactionItemResponse> getTransactionItemsByTransactionId(UUID transactionId, PaginationRequest paginationRequest);
 
-    TransactionItem getTransactionByTransactionIdAndProductId(UUID transactionId, Long ProductId);
+    TransactionItemResponse getTransactionByTransactionIdAndProductId(UUID transactionId, Long ProductId);
 }
--- src/main/java/lumi/insert/app/service/TransactionPaymentService.java
+++ src/main/java/lumi/insert/app/service/TransactionPaymentService.java
@@ -1,20 +1,21 @@
 package lumi.insert.app.service;
 
 import java.util.UUID;
-
-import org.springframework.data.domain.Pageable;
+ 
 import org.springframework.data.domain.Slice;
 
-import lumi.insert.app.dto.request.TransactionPaymentCreateRequest; 
+import lumi.insert.app.dto.request.PaginationRequest;
+import lumi.insert.app.dto.request.TransactionPaymentCreateRequest;
+import lumi.insert.app.dto.request.TransactionPaymentGetByFilter;
 import lumi.insert.app.dto.response.TransactionPaymentResponse;
 
 public interface TransactionPaymentService {
 
     TransactionPaymentResponse createTransactionPayment(UUID transactionId, TransactionPaymentCreateRequest request);
 
-    Slice<TransactionPaymentResponse> getTransactionsByTransactionId(UUID transactionId);
+    Slice<TransactionPaymentResponse> getTransactionPaymentsByTransactionId(UUID transactionId, PaginationRequest request);
 
-    TransactionPaymentResponse getTransaction(UUID id);
+    TransactionPaymentResponse getTransactionPayment(UUID id);
 
-    Slice<TransactionPaymentResponse> getTransactionsByRequests(TransactionPaymentCreateRequest filter, Pageable pageable);
+    Slice<TransactionPaymentResponse> getTransactionPaymentsByRequests(TransactionPaymentGetByFilter request);
 }
