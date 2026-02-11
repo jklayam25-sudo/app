59aba1d7917c13f4bb342b70451414535568705c
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-12 03:40:57 +0700

    feat(trx): add GET > search transactions use filtering param

--- src/main/java/lumi/insert/app/controller/TransactionController.java
+++ src/main/java/lumi/insert/app/controller/TransactionController.java
@@ -4,9 +4,11 @@ import java.net.URI;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import lumi.insert.app.controller.wrapper.WebResponse; 
-import lumi.insert.app.dto.request.TransactionCreateRequest; 
+import lumi.insert.app.dto.request.TransactionCreateRequest;
+import lumi.insert.app.dto.request.TransactionGetByFilter;
 import lumi.insert.app.dto.response.TransactionResponse;
 import lumi.insert.app.service.TransactionService;
 
@@ -52,6 +55,18 @@ public class TransactionController {
         TransactionResponse resultFromService = transactionService.getTransaction(id);
         
         WebResponse<TransactionResponse> wrappedResult = WebResponse.getWrapper(resultFromService, null);
+
+        return ResponseEntity.ok(wrappedResult);
+    }
+
+    @GetMapping(
+        path = "/api/transactions/filter",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<WebResponse<Slice<TransactionResponse>>> getTransactions(@ModelAttribute TransactionGetByFilter request){
+        Slice<TransactionResponse> resultFromService = transactionService.searchTransactionsByRequests(request);
+        
+        WebResponse<Slice<TransactionResponse>> wrappedResult = WebResponse.getWrapper(resultFromService, null);
 
         return ResponseEntity.ok(wrappedResult);
     }
