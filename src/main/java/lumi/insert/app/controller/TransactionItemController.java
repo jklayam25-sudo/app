ab8ddb3fafd988500aed47249f51e125d084b11e
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-13 04:40:46 +0700

    feat(trxItem) : add controller for endpoint DELETE transaction items, POST change trx items quantity, POST refund trx items

--- src/main/java/lumi/insert/app/controller/TransactionItemController.java
+++ src/main/java/lumi/insert/app/controller/TransactionItemController.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ModelAttribute; 
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping; 
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -19,6 +21,7 @@ import jakarta.validation.Valid;
 import lumi.insert.app.controller.wrapper.WebResponse;
 import lumi.insert.app.dto.request.PaginationRequest;
 import lumi.insert.app.dto.request.TransactionItemCreateRequest;
+import lumi.insert.app.dto.response.TransactionItemDelete;
 import lumi.insert.app.dto.response.TransactionItemResponse; 
 import lumi.insert.app.service.TransactionItemService;
 
@@ -76,6 +79,45 @@ public class TransactionItemController {
     )
     ResponseEntity<WebResponse<TransactionItemResponse>> getTransactionItemByProductId(@PathVariable(name = "transactionId") UUID transactionId, @PathVariable(name = "productId") Long productId){
         TransactionItemResponse resultFromService = transactionItemService.getTransactionByTransactionIdAndProductId(transactionId, productId);
+ 
+        WebResponse<TransactionItemResponse> wrappedResult = WebResponse.getWrapper(resultFromService, null);
+
+        return ResponseEntity.ok(wrappedResult);   
+    }
+
+    @DeleteMapping(
+        path = "/api/transactions/{transactionId}/items/{id}",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<WebResponse<TransactionItemDelete>> DeleteTransactionItem(@PathVariable(name = "transactionId") UUID transactionId, @PathVariable(name = "id") UUID id){
+        TransactionItemDelete resultFromService = transactionItemService.deleteTransactionItem(id);
+ 
+        WebResponse<TransactionItemDelete> wrappedResult = WebResponse.getWrapper(resultFromService, null);
+
+        return ResponseEntity.status(HttpStatusCode.valueOf(410)).body(wrappedResult);   
+    }
+
+    @PostMapping(
+        path = "/api/transactions/{transactionId}/items/{id}/quantity",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
+    )
+    ResponseEntity<WebResponse<TransactionItemResponse>> UpdateTransactionItemQuantity(@PathVariable(name = "transactionId") UUID transactionId, @PathVariable(name = "id") UUID id, @RequestParam(name = "quantity") Long quantity){
+        TransactionItemResponse resultFromService = transactionItemService.updateTransactionItemQuantity(id, quantity);
+ 
+        WebResponse<TransactionItemResponse> wrappedResult = WebResponse.getWrapper(resultFromService, null);
+
+
+        return ResponseEntity.ok(wrappedResult);   
+    }
+
+    @PostMapping(
+        path = "/api/transactions/{transactionId}/items/{id}/refund",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
+    )
+    ResponseEntity<WebResponse<TransactionItemResponse>> refundTransactionItem(@PathVariable(name = "transactionId") UUID transactionId, @PathVariable(name = "id") UUID id, @RequestParam(name = "quantity") Long quantity){
+        TransactionItemResponse resultFromService = transactionItemService.refundTransactionItem(id, quantity);
  
         WebResponse<TransactionItemResponse> wrappedResult = WebResponse.getWrapper(resultFromService, null);
 
