f2e5ec719c3800141aaecc8e85ff4fcb2af922d8
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-12 07:07:25 +0700

    feat(trx): add controller POST > create transaction item feature

---
+++ src/main/java/lumi/insert/app/controller/TransactionItemController.java
@@ -0,0 +1,43 @@
+package lumi.insert.app.controller;
+
+import java.net.URI;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
+import lumi.insert.app.controller.wrapper.WebResponse; 
+import lumi.insert.app.dto.request.TransactionItemCreateRequest;
+import lumi.insert.app.dto.response.TransactionItemResponse; 
+import lumi.insert.app.service.TransactionItemService;
+
+@RestController
+public class TransactionItemController {
+    
+    @Autowired
+    TransactionItemService transactionItemService;
+
+    @PostMapping(
+        path = "/api/transactions/{id}/items",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
+    )
+    ResponseEntity<WebResponse<TransactionItemResponse>> createTransactionItem(@PathVariable(name = "id") UUID id, @Valid TransactionItemCreateRequest request){
+        TransactionItemResponse resultFromService = transactionItemService.createTransactionItem(id, request);
+ 
+        WebResponse<TransactionItemResponse> wrappedResult = WebResponse.getWrapper(resultFromService, null);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/" + resultFromService.id())
+        .buildAndExpand(resultFromService.id())
+        .toUri();
+
+        return ResponseEntity.created(location).body(wrappedResult);   
+    }
+}
