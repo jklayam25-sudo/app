0aab3b60bac6a2f6f450c30ac366d28dbf63a734
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-12 07:10:25 +0700

    refactor(trx): remove unnecessary import

--- src/test/java/lumi/insert/app/controller/transaction/TransactionControllerUpdateTest.java
+++ src/test/java/lumi/insert/app/controller/transaction/TransactionControllerUpdateTest.java
@@ -8,15 +8,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
+ 
 import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test; 
 import org.springframework.http.MediaType;
-
-import lumi.insert.app.dto.response.TransactionResponse;
+ 
 import lumi.insert.app.exception.ForbiddenRequestException;
 import lumi.insert.app.exception.NotFoundEntityException;
  
