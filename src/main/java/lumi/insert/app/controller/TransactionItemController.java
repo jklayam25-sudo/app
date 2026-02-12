ecec631c84db96d8051fba2401a99ae85882f286
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-12 23:15:24 +0700

    feat(trx): add controller GET > get transaction item by ID feature

--- src/main/java/lumi/insert/app/controller/TransactionItemController.java
+++ src/main/java/lumi/insert/app/controller/TransactionItemController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +40,17 @@ public class TransactionItemController {
         .toUri();
 
         return ResponseEntity.created(location).body(wrappedResult);   
+    }
+
+    @GetMapping(
+        path = "/api/transactions/{id}/items",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<WebResponse<TransactionItemResponse>> getTransactionItem(@PathVariable(name = "id") UUID id){
+        TransactionItemResponse resultFromService = transactionItemService.getTransactionItem(id);
+ 
+        WebResponse<TransactionItemResponse> wrappedResult = WebResponse.getWrapper(resultFromService, null);
+
+        return ResponseEntity.ok(wrappedResult);   
     }
 }
