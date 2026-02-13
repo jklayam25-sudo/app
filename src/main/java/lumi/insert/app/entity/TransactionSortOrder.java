204322a4533f1a489c8d4f4cdc5df44bf3edf315
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-14 01:36:22 +0700

    feat(trx) add enum for sortby filtering

---
+++ src/main/java/lumi/insert/app/entity/TransactionSortOrder.java
@@ -0,0 +1,15 @@
+package lumi.insert.app.entity;
+
+public enum TransactionSortOrder {
+    createdAt,
+    updatedAt,
+    totalItems,
+    totalFee,
+    totalDiscount,
+    subTotal,
+    grandTotal,
+    totalUnpaid,
+    totalPaid,
+    totalUnrefunded,
+    totalRefunded;
+}
