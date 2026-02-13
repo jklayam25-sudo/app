5cd21412f63cfc381f89e9512522d19917a10ecd
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-14 01:34:28 +0700

    fix(product): add annotation validate to product filtering request dto

--- src/main/java/lumi/insert/app/dto/request/ProductGetByFilter.java
+++ src/main/java/lumi/insert/app/dto/request/ProductGetByFilter.java
@@ -1,5 +1,7 @@
 package lumi.insert.app.dto.request;
  
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,16 +20,21 @@ public class ProductGetByFilter extends PaginationRequest {
     String name = "";
 
     @Builder.Default
+    @Min(value = 0, message = "minPrice minimal value is 0")
     Long minPrice = 0L;
 
     @Builder.Default
+    @Min(value = 0, message = "maxPrice minimal value is 0")
     Long maxPrice = 999999999L;
     
     Long categoryId;
 
     @Builder.Default
+    @Pattern(regexp = "createdAt|updatedAt|sellPrice|basePrice|stockQuantity", message = "check documentation for sortBy specification")
     String sortBy = "sellPrice";
 
-    String sortDirection;
+    @Builder.Default
+    @Pattern(regexp = "DESC|ASC", message = "check documentation for sortDirection specification")
+    String sortDirection = "DESC";
 
 }
