847d491753b47a4fdf13127460b4df03337e0152
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-14 01:35:17 +0700

    fix(trxPayment): add annotation validate to transaction payment filtering request dto

--- src/main/java/lumi/insert/app/dto/request/TransactionPaymentGetByFilter.java
+++ src/main/java/lumi/insert/app/dto/request/TransactionPaymentGetByFilter.java
@@ -3,6 +3,8 @@ package lumi.insert.app.dto.request;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,9 +22,11 @@ public class TransactionPaymentGetByFilter extends PaginationRequest{
     UUID transactionId;
 
     @Builder.Default
+    @Min(value = 0, message = "minTotalPayment minimal value is 0")
     Long minTotalPayment = 0L;
 
     @Builder.Default
+    @Min(value = 0, message = "maxTotalPayment minimal value is 0")
     Long maxTotalPayment = 9999999999990L;
 
     LocalDateTime minCreatedAt;
@@ -30,9 +34,11 @@ public class TransactionPaymentGetByFilter extends PaginationRequest{
     LocalDateTime maxCreatedAt;
 
     @Builder.Default
+    @Pattern(regexp = "createdAt|updatedAt|totalPayment", message = "check documentation for sortBy specification")
     String sortBy = "createdAt";
 
     @Builder.Default
+    @Pattern(regexp = "DESC|ASC", message = "check documentation for sortDirection specification")
     String sortDirection = "DESC";
 
 }
