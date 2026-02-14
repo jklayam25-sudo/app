913d13db2aa6398837a044820b7e2ffed8dffcae
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-14 07:45:07 +0700

    feat(employee): add create request dto

---
+++ src/main/java/lumi/insert/app/dto/request/EmployeeCreateRequest.java
@@ -0,0 +1,23 @@
+package lumi.insert.app.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class EmployeeCreateRequest {
+    
+    @NotBlank(message = "username cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z0-9]{5,30}$", message = "Username has to be 5-30 length and doesn't contain unique char")
+    private String username;
+
+    @NotBlank(message = "fullname cannot be empty")
+    private String fullname;
+
+    @NotBlank(message = "password cannot be empty")
+    @Pattern(regexp = "^(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{5,50}$", message = "password has to be 5-50 length and has atleast 1 unique char")
+    private String password;
+
+}
