4facf12c49ab80eb78900e9b7b302f15643de79f
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-16 02:09:35 +0700

    feat(employee): add employee response dto

--- src/main/java/lumi/insert/app/dto/response/EmployeeResponse.java
+++ src/main/java/lumi/insert/app/dto/response/EmployeeResponse.java
@@ -1,9 +1,9 @@
 package lumi.insert.app.dto.response;
 
 import java.util.UUID;
-
+ 
 import lumi.insert.app.entity.nondatabase.EmployeeRole;
-
+ 
 public record EmployeeResponse(UUID id, String username, String fullname, EmployeeRole role) {
-    
+ 
 }
