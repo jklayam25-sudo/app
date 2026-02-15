33a482c9124144185cf14e0b84de5ba1ef2d20d1
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-16 02:12:20 +0700

    feat(Employe): add updateEmployee nonNull mapper

--- src/main/java/lumi/insert/app/utils/mapper/EmployeeMapper.java
+++ src/main/java/lumi/insert/app/utils/mapper/EmployeeMapper.java
@@ -1,10 +1,12 @@
 package lumi.insert.app.utils.mapper;
 
 import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
+import org.mapstruct.Mapper; 
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
+import lumi.insert.app.dto.request.EmployeeUpdateRequest; 
 import lumi.insert.app.dto.response.EmployeeResponse; 
 import lumi.insert.app.entity.Employee; 
 
@@ -14,4 +16,6 @@ public interface EmployeeMapper {
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
     EmployeeResponse createDtoResponseFromEmployee(Employee entity);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE) 
+    void updateEmployeeFromDto(EmployeeUpdateRequest dto, @MappingTarget Employee entity);
 }
