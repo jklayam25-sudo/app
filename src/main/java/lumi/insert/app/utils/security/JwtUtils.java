0511b46ba3ba1d7176f9b48263d335b99bd5b9f4
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-21 18:48:40 +0700

    add(jwt): add component utils for jwt function

---
+++ src/main/java/lumi/insert/app/utils/security/JwtUtils.java
@@ -0,0 +1,41 @@
+package lumi.insert.app.utils.security;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import lumi.insert.app.entity.Employee;
+
+@Component
+public class JwtUtils {
+
+    Algorithm algorithm = Algorithm.HMAC256("SOME_THING-iusedto_knOW");
+    
+    String issuer = "LUMI-INSERT";
+
+    JWTVerifier jwtVerifier = JWT.require(algorithm)
+            .withIssuer(issuer).build();
+
+    public String getAccessToken(Employee employee){
+        String accessToken = JWT.create()
+        .withIssuer(issuer)
+        .withIssuedAt(Instant.now())
+        .withClaim("username", employee.getUsername())
+        .withClaim("role", employee.getRole().toString()) 
+        .withExpiresAt(Instant.now().plus(15, ChronoUnit.MINUTES))
+        .sign(algorithm);
+
+        return accessToken;
+    }
+
+    public DecodedJWT parseAccessToken(String accessToken){
+        return jwtVerifier.verify(accessToken);
+    }
+
+}
