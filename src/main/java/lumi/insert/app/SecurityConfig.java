30529c39cc046bb6d5a0f84ee049c3574d26698b
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-24 18:52:48 +0700

    remove comment

--- src/main/java/lumi/insert/app/SecurityConfig.java
+++ src/main/java/lumi/insert/app/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) // Ini cara mematikannya
+            .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         
         return http.build();
