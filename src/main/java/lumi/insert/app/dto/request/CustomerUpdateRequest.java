5cecf01a104e469842cca93996bcf5d130ed1288
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-24 18:56:12 +0700

    fix(customer): fix update dto parent annotation bug > update should be optional but parent field is required

--- src/main/java/lumi/insert/app/dto/request/CustomerUpdateRequest.java
+++ src/main/java/lumi/insert/app/dto/request/CustomerUpdateRequest.java
@@ -1,17 +1,33 @@
 package lumi.insert.app.dto.request; 
+import jakarta.validation.constraints.Email; 
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
 @SuperBuilder
-public class CustomerUpdateRequest extends CustomerCreateRequest{
-    
-    Boolean isActive;
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerUpdateRequest{
+     
+    @Pattern(regexp = "^[a-zA-Z0-9.-_ ]{5,30}$", message = "name has to be 5-30 length")
+    private String name;
+ 
+    @Email(message = "email doesn't meet the email format")
+    private String email;
+ 
+    private String contact;
+ 
+    private String shippingAddress;
 
-    Double Latitude;
+    private Boolean isActive;
 
-    Double Longitude;
+    private Double Latitude;
+
+    private Double Longitude;
 
 }
