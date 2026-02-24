b8e40923329d20a140737b24ca2196362d3c4c7d
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-24 19:02:20 +0700

    test: add test to  duplicate entity issue

--- src/test/java/lumi/insert/app/service/customer/CustomerServiceEditTest.java
+++ src/test/java/lumi/insert/app/service/customer/CustomerServiceEditTest.java
@@ -3,6 +3,7 @@ package lumi.insert.app.service.customer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 
 import lumi.insert.app.dto.request.CustomerUpdateRequest;
 import lumi.insert.app.dto.response.CustomerDetailResponse;
+import lumi.insert.app.exception.DuplicateEntityException;
 import lumi.insert.app.exception.NotFoundEntityException;
 
 public class CustomerServiceEditTest extends BaseCustomerServiceTest{
@@ -19,6 +21,7 @@ public class CustomerServiceEditTest extends BaseCustomerServiceTest{
     @Test
     @DisplayName("Should return new updated Customer when update success")
     void updateCustomer_validRequest_ReturnUpdatedDTO(){
+        when(customerRepository.existsByName(anyString())).thenReturn(false);
         when(customerRepository.findById(setupCustomer.getId())).thenReturn(Optional.of(setupCustomer));
 
         CustomerUpdateRequest request = CustomerUpdateRequest.builder()
@@ -35,6 +38,7 @@ public class CustomerServiceEditTest extends BaseCustomerServiceTest{
     @Test
     @DisplayName("Should thorw NotFoundEntity Exc when requested customer is not found")
     void updateCustomer_notFound_throwNotFound(){
+        when(customerRepository.existsByName(anyString())).thenReturn(false);
         when(customerRepository.findById(setupCustomer.getId())).thenReturn(Optional.empty());
 
         CustomerUpdateRequest request = CustomerUpdateRequest.builder()
@@ -43,6 +47,18 @@ public class CustomerServiceEditTest extends BaseCustomerServiceTest{
         .build();
 
         assertThrows(NotFoundEntityException.class, () -> customerServiceMock.updateCustomer(setupCustomer.getId(), request)) ;
- 
+    }
+
+    @Test
+    @DisplayName("Should thorw DuplicateEntityException Exc when requested update name is exists")
+    void updateCustomer_duplicateEntity_throwDuplicate(){
+        when(customerRepository.existsByName("new Name LTE")).thenReturn(true);
+
+        CustomerUpdateRequest request = CustomerUpdateRequest.builder()
+        .name("new Name LTE")
+        .isActive(false)
+        .build();
+
+        assertThrows(DuplicateEntityException.class, () -> customerServiceMock.updateCustomer(setupCustomer.getId(), request)) ;
     }
 }
