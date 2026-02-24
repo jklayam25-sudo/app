40552c5068194f55dab9649ad39e73e478675210
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-24 12:06:55 +0700

    utils: implement utils to get specification < trx, trxpayment, product

---
+++ src/main/java/lumi/insert/app/utils/generator/JpaSpecGenerator.java
@@ -0,0 +1,118 @@
+package lumi.insert.app.utils.generator;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.criteria.Predicate;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import lumi.insert.app.dto.request.CustomerGetByFilter;
+import lumi.insert.app.dto.request.PaginationRequest;
+import lumi.insert.app.dto.request.ProductGetByFilter;
+import lumi.insert.app.dto.request.TransactionGetByFilter;
+import lumi.insert.app.dto.request.TransactionPaymentGetByFilter;
+import lumi.insert.app.entity.Customer;
+import lumi.insert.app.entity.Product;
+import lumi.insert.app.entity.Transaction;
+import lumi.insert.app.entity.TransactionPayment;
+
+@Component
+public class JpaSpecGenerator {
+    
+    public Pageable pageable(PaginationRequest request){
+        Sort sort = Sort.by(request.getSortBy());
+
+        if(request.getSortDirection().equalsIgnoreCase("DESC")){
+            sort = sort.descending();
+        } else {
+            sort = sort.ascending();
+        }
+
+        return PageRequest.of(request.getPage(), request.getSize(), sort);
+    }
+
+    public Specification<Customer> customerSpecification(CustomerGetByFilter request){
+        Specification<Customer> specifications = (root, criteria, builder) -> {
+            List<Predicate> predicates = new ArrayList<Predicate>();
+
+            if(request.getName() != null){
+                predicates.add(builder.equal(root.get("name"), request.getName()));
+            }
+            if(request.getContact() != null){
+                predicates.add(builder.equal(root.get("contact"), request.getContact()));
+            }
+            if(request.getEmail() != null){
+                predicates.add(builder.equal(root.get("email"), request.getEmail()));
+            }
+            if(request.getIsActive() != null){
+                predicates.add(builder.equal(root.get("isActive"), request.getIsActive()));
+            }
+
+            predicates.add(builder.between(root.get("totalTransaction"), request.getMinTotalTransaction(), request.getMaxTotalTransaction()));
+            predicates.add(builder.between(root.get("totalUnpaid"), request.getMinTotalUnpaid(), request.getMaxTotalUnpaid()));
+            predicates.add(builder.between(root.get("totalPaid"), request.getMinTotalPaid(), request.getMaxTotalPaid())); 
+            return builder.and(predicates);
+        };
+        
+        return specifications;
+    }
+
+    public Specification<Product> productSpecification(ProductGetByFilter request){
+        Specification<Product> specification = (root, criteria, builder) -> {
+            List<Predicate> predicates = new ArrayList<Predicate>();
+
+            if(request.getCategoryId() != null){
+                predicates.add(builder.equal(root.get("category").get("id"), request.getCategoryId()));
+            }
+            if (request.getName() != null) {
+                predicates.add(builder.like(builder.lower(root.get("name")), "%" + request.getName() + "%"));
+            }
+            predicates.add(builder.isTrue(root.get("isActive")));
+            predicates.add(builder.between(root.get("sellPrice"), request.getMinPrice(), request.getMaxPrice()));
+
+            return builder.and(predicates);
+        }; 
+        return specification;
+    }
+
+    public Specification<Transaction> transactionSpecification(TransactionGetByFilter request){
+        Specification<Transaction> specification = (root, criteria, builder) -> {
+            List<Predicate> predicates = new ArrayList<Predicate>();
+
+            if(request.getStatus() != null){
+                predicates.add(builder.equal(root.get("status"), request.getStatus()));
+            }
+            if(request.getCustomerId() != null){
+                predicates.add(builder.equal(root.get("customer").get("id"), request.getCustomerId()));
+            }
+            if (request.getMinCreatedAt() != null && request.getMaxCreatedAt() != null) {
+                predicates.add(builder.between(root.get("createdAt"), request.getMinCreatedAt(), request.getMaxCreatedAt()));
+            } 
+            predicates.add(builder.between(root.get("totalItems"), request.getMinTotalItems(), request.getMaxTotalItems()));
+            predicates.add(builder.between(root.get("grandTotal"), request.getMinGrandTotal(), request.getMaxGrandTotal()));
+            predicates.add(builder.between(root.get("totalUnpaid"), request.getMinTotalUnpaid(), request.getMaxTotalUnpaid()));
+            predicates.add(builder.between(root.get("totalPaid"), request.getMinTotalPaid(), request.getMaxTotalPaid()));
+
+            return builder.and(predicates);
+        }; 
+        return specification;
+    }
+
+    public Specification<TransactionPayment> transactionPaymentSpecification(TransactionPaymentGetByFilter request){
+        Specification<TransactionPayment> specification = (root, criteria, builder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if(request.getTransactionId() != null) predicates.add(builder.equal(root.get("transaction").get("id"), request.getTransactionId()));
+            if(request.getMinCreatedAt() != null && request.getMaxCreatedAt() != null) predicates.add(builder.between(root.get("createdAt"), request.getMinCreatedAt(), request.getMaxCreatedAt()));
+
+            predicates.add(builder.between(root.get("totalPayment"), request.getMinTotalPayment(), request.getMaxTotalPayment()));
+
+            return builder.and(predicates);
+        };
+        return specification;
+    }
+}
