9483fb05130714b84ff29db128e4b9a15ef0ac83
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-25 18:29:26 +0700

    test(memo): fix test due to repo change

--- src/test/java/lumi/insert/app/repository/MemoRepositoryTest.java
+++ src/test/java/lumi/insert/app/repository/MemoRepositoryTest.java
@@ -1,6 +1,8 @@
 package lumi.insert.app.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals; 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -8,9 +10,13 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.data.domain.Slice;
 
 import jakarta.transaction.Transactional;
+import lumi.insert.app.dto.response.MemoResponse; 
+import lumi.insert.app.entity.Employee;
 import lumi.insert.app.entity.Memo;
+import lumi.insert.app.entity.MemoView;
 import lumi.insert.app.entity.nondatabase.EmployeeRole;
 
 @DataJpaTest
@@ -19,6 +25,12 @@ public class MemoRepositoryTest {
     
     @Autowired
     MemoRepository memoRepository;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    MemoViewRepository memoViewRepository;
 
     @Test
     void getMemo_roleALL_returnALL(){
@@ -41,11 +53,11 @@ public class MemoRepositoryTest {
 
         memoRepository.saveAllAndFlush(List.of(cashierMemo, financeMemo, allMemo));
 
-        List<Memo> activeMemosByRoleOrPublic = memoRepository.findActiveMemosByRoleOrPublic(EmployeeRole.FINANCE, LocalDateTime.now().minusHours(1));
+        Slice<MemoResponse> activeMemosByRoleOrPublic = memoRepository.findActiveMemosByRoleOrPublic(null, EmployeeRole.FINANCE, LocalDateTime.now().minusHours(1));
 
-        assertEquals(2, activeMemosByRoleOrPublic.size());
-        assertEquals(financeMemo.getBody(), activeMemosByRoleOrPublic.getFirst().getBody());
-        assertEquals(allMemo.getBody(), activeMemosByRoleOrPublic.getLast().getBody());
+        assertEquals(2, activeMemosByRoleOrPublic.getNumberOfElements());
+        assertEquals(financeMemo.getBody(), activeMemosByRoleOrPublic.getContent().getFirst().body()); 
+        assertEquals(allMemo.getBody(), activeMemosByRoleOrPublic.getContent().getLast().body());
     }
 
     @Test
@@ -65,10 +77,11 @@ public class MemoRepositoryTest {
 
         memoRepository.saveAllAndFlush(List.of(cashierMemo, allMemo));
 
-        List<Memo> activeMemosByRoleOrPublic = memoRepository.findActiveMemosByRoleOrPublic(EmployeeRole.CASHIER, LocalDateTime.now().minusHours(1));
+        Slice<MemoResponse> activeMemosByRoleOrPublic = memoRepository.findActiveMemosByRoleOrPublic(null, EmployeeRole.CASHIER, LocalDateTime.now().minusHours(1));
 
-        assertEquals(1, activeMemosByRoleOrPublic.size()); 
-        assertEquals(allMemo.getBody(), activeMemosByRoleOrPublic.getLast().getBody());
+        assertEquals(1, activeMemosByRoleOrPublic.getNumberOfElements()); 
+        assertEquals(allMemo.getBody(), activeMemosByRoleOrPublic.getContent().getLast().body());
+        assertFalse(activeMemosByRoleOrPublic.getContent().getFirst().isRead());
     }
 
     @Test
@@ -84,5 +97,32 @@ public class MemoRepositoryTest {
         int updateIsActiveFalse = memoRepository.updateIsActiveFalse(saveAndFlush.getId());
 
         assertEquals(1, updateIsActiveFalse);
+    }
+
+    @Test
+    void findActiveMemosByRoleOrPublic_isReadTest_returnTrue(){
+        Memo cashierMemo = Memo.builder() 
+        .title("Cashier") 
+        .body("Cashier body")
+        .role(EmployeeRole.CASHIER)
+        .build();
+ 
+        Memo savedMemo = memoRepository.saveAndFlush(cashierMemo);;
+
+        Employee employee = Employee.builder()
+        .username("TESTEMPLOYE")
+        .fullname("TESTEMPLOYE")
+        .password("TESTEMPLOYE")
+        .joinDate(LocalDateTime.now())
+        .build();
+
+        Employee savedEmployee = employeeRepository.saveAndFlush(employee);
+
+        memoViewRepository.saveAndFlush(new MemoView(savedMemo, savedEmployee));
+
+        Slice<MemoResponse> activeMemosByRoleOrPublic = memoRepository.findActiveMemosByRoleOrPublic(savedEmployee.getId(), EmployeeRole.CASHIER, LocalDateTime.now().minusHours(1));
+
+        assertEquals(1, activeMemosByRoleOrPublic.getNumberOfElements());  
+        assertTrue(activeMemosByRoleOrPublic.getContent().getFirst().isRead());
     }
 } 
