72b8faa07188834c1f2e2a69c920fcb07bb641f1
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-27 19:07:00 +0700

    test(stockCard): add test for specification find query

--- src/test/java/lumi/insert/app/repository/StockCardRepositoryTest.java
+++ src/test/java/lumi/insert/app/repository/StockCardRepositoryTest.java
@@ -1,6 +1,7 @@
 package lumi.insert.app.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,17 +11,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.context.annotation.Import; 
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.domain.Specification;
 
 import jakarta.transaction.Transactional;
+import lumi.insert.app.dto.request.StockCardGetByFilter;
 import lumi.insert.app.dto.response.StockCardResponse;
 import lumi.insert.app.entity.Product;
 import lumi.insert.app.entity.StockCard;
 import lumi.insert.app.entity.nondatabase.StockMove;
+import lumi.insert.app.utils.generator.JpaSpecGenerator;
 
 @DataJpaTest
 @Transactional
+@Import(JpaSpecGenerator.class)
 public class StockCardRepositoryTest {
     
     @Autowired
@@ -28,6 +35,9 @@ public class StockCardRepositoryTest {
 
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    JpaSpecGenerator jpaSpecGenerator;
 
     Product product;
 
@@ -60,11 +70,12 @@ public class StockCardRepositoryTest {
         .basePrice(1000L)
         .build();
 
-        List<StockCard> saveAllAndFlush = stockCardRepository.saveAllAndFlush(List.of(stockCard1, stockCard2));;
+        stockCardRepository.saveAndFlush(stockCard1);
+        StockCard saveAndFlush2 = stockCardRepository.saveAndFlush(stockCard2);;
 
         Slice<StockCardResponse> byIndexPagination = stockCardRepository.findByIndexPagination(LocalDateTime.now().minusDays(1), LocalDateTime.now(), null, PageRequest.of(0, 2));
         assertEquals(2, byIndexPagination.getNumberOfElements());
-        assertEquals(saveAllAndFlush.getLast().getId(), byIndexPagination.getContent().getLast().id());
+        assertEquals(saveAndFlush2.getId(), byIndexPagination.getContent().getLast().id());
     }
 
     @Test
@@ -91,12 +102,52 @@ public class StockCardRepositoryTest {
         .basePrice(1000L)
         .build();
 
-        List<StockCard> saveAllAndFlush = stockCardRepository.saveAllAndFlush(List.of(stockCard1, stockCard2));;
+        StockCard saveAndFlush = stockCardRepository.saveAndFlush(stockCard1);
+        StockCard saveAndFlush2 = stockCardRepository.saveAndFlush(stockCard2);;
 
-        Slice<StockCardResponse> byIndexPagination = stockCardRepository.findByIndexPagination(LocalDateTime.now().minusDays(1), LocalDateTime.now(), saveAllAndFlush.getFirst().getId(), PageRequest.of(0, 2));
+        Slice<StockCardResponse> byIndexPagination = stockCardRepository.findByIndexPagination(LocalDateTime.now().minusDays(1), LocalDateTime.now(), saveAndFlush.getId(), PageRequest.of(0, 2));
         assertEquals(1, byIndexPagination.getNumberOfElements());
-        assertEquals(saveAllAndFlush.getLast().getId(), byIndexPagination.getContent().getLast().id());
+        assertEquals(saveAndFlush2.getId(), byIndexPagination.getContent().getLast().id());
     }
 
-    
+    @Test
+    void findAllSpecification_foundEntityFilterLastId_returnSliceDTO(){
+        StockCard stockCard1 = StockCard.builder()
+        .referenceId(UUID.randomUUID())
+        .product(product)
+        .productName(product.getName())
+        .quantity(-5L)
+        .oldStock(10L)
+        .newStock(5L)
+        .type(StockMove.PURCHASE)
+        .basePrice(1000L)
+        .build();
+
+        StockCard stockCard2 = StockCard.builder()
+        .referenceId(UUID.randomUUID())
+        .product(product)
+        .productName(product.getName())
+        .quantity(-5L)
+        .oldStock(10L)
+        .newStock(5L)
+        .type(StockMove.SALE)
+        .basePrice(1000L)
+        .build();
+
+        List<StockCard> saveAllAndFlush = stockCardRepository.saveAllAndFlush(List.of(stockCard1, stockCard2));;
+
+        StockCardGetByFilter request = StockCardGetByFilter.builder()
+        .type(StockMove.PURCHASE)
+        .build();
+
+        Pageable pageable = jpaSpecGenerator.pageable(request);
+        Specification<StockCard> stockCardSpecification = jpaSpecGenerator.stockCardSpecification(request);
+
+        Slice<StockCard> slices = stockCardRepository.findAll(stockCardSpecification, pageable);;
+        assertEquals(1, slices.getNumberOfElements());
+        assertTrue(slices.isLast());
+        assertEquals(saveAllAndFlush.getFirst().getId(), slices.getContent().getLast().getId());
+    }
+
+
 }
