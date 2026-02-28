a02f625ddaec8927cb4fb1a1608bcf56a33cddfc
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-02-28 15:27:20 +0700

    feat(stockcard): update validation to DTO request

--- src/main/java/lumi/insert/app/dto/request/StockCardCreateRequest.java
+++ src/main/java/lumi/insert/app/dto/request/StockCardCreateRequest.java
@@ -1,10 +1,10 @@
 package lumi.insert.app.dto.request;
 
 import java.util.UUID;
- 
-import jakarta.validation.constraints.Min;
+  
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data; 
 
@@ -13,17 +13,17 @@ import lombok.Data;
 @Builder
 public class StockCardCreateRequest {
     
-    @NotBlank(message = "referenceId cannot be empty") 
+    @NotNull(message = "referenceId cannot be empty") 
     private UUID referenceId;
  
     @NotNull(message = "productId cannot be empty")
     private Long productId; 
  
-    @NotNull(message = "quantity cannot be empty and value atleas 1")
-    @Min(value = 1)
+    @NotNull(message = "quantity cannot be empty and value atleas 1") 
     private Long quantity; 
  
     @NotBlank(message = "type cannot be empty")
+    @Pattern(regexp = "CUSTOMER_IN|CUSTOMER_OUT|DEFECT|REPAIRED|SUPPLIER_IN|SUPPLIER_OUT|", message = "check documentation for type")
     private String type;
 
     private String description;
