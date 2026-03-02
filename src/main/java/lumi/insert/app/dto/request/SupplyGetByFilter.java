31438e93d55bfa5bd7d3da33d3c95174192f301a
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-02 18:41:14 +0700

    feat(allSupply): add supply relate request filter DTO

---
+++ src/main/java/lumi/insert/app/dto/request/SupplyGetByFilter.java
@@ -0,0 +1,71 @@
+package lumi.insert.app.dto.request;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import lumi.insert.app.entity.nondatabase.SupplyStatus; 
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+public class SupplyGetByFilter extends PaginationRequest{
+
+    @Builder.Default
+    @Min(value = 0, message = "minTotalItems minimal value is 0")
+    Long minTotalItems = 0L;
+
+    @Builder.Default
+    @Min(value = 0, message = "maxTotalItems minimal value is 0")
+    Long maxTotalItems = 9999990L;
+
+    @Builder.Default
+    @Min(value = 0, message = "minGrandTotal minimal value is 0")
+    Long minGrandTotal = 0L;
+
+    @Builder.Default
+    @Min(value = 0, message = "maxGrandTotal minimal value is 0")
+    Long maxGrandTotal = 9999999999990L;
+
+    @Builder.Default
+    @Min(value = 0, message = "minTotalUnpaid minimal value is 0")
+    Long minTotalUnpaid = 0L;
+
+    @Builder.Default
+    @Min(value = 0, message = "maxTotalUnpaid minimal value is 0")
+    Long maxTotalUnpaid = 9999999999990L;
+
+    @Builder.Default
+    @Min(value = 0, message = "minTotalPaid minimal value is 0")
+    Long minTotalPaid = 0L;
+
+    @Builder.Default
+    @Min(value = 0, message = "maxTotalPaid minimal value is 0")
+    Long maxTotalPaid = 9999999999990L;
+
+    SupplyStatus status;
+
+    UUID supplierId;
+
+    LocalDateTime minCreatedAt;
+
+    LocalDateTime maxCreatedAt;
+
+    @Builder.Default
+    @Pattern(regexp = "createdAt|updatedAt|totalItems|totalFee|totalDiscount|subTotal|grandTotal|totalUnpaid|totalPaid|totalUnrefunded|totalRefunded", message = "check documentation for sortBy specification")
+    String sortBy = "createdAt";
+
+    @Builder.Default
+    @Pattern(regexp = "DESC|ASC", message = "check documentation for sortDirection specification")
+    String sortDirection = "DESC";
+
+}
---
+++ src/main/java/lumi/insert/app/dto/request/SupplyPaymentGetByFilter.java
@@ -0,0 +1,44 @@
+package lumi.insert.app.dto.request;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder; 
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+public class SupplyPaymentGetByFilter extends PaginationRequest{
+ 
+    UUID supplyId;
+
+    @Builder.Default
+    @Min(value = 0, message = "minTotalPayment minimal value is 0")
+    Long minTotalPayment = 0L;
+
+    @Builder.Default
+    @Min(value = 0, message = "maxTotalPayment minimal value is 0")
+    Long maxTotalPayment = 9999999999990L;
+
+    LocalDateTime minCreatedAt;
+
+    LocalDateTime maxCreatedAt;
+
+    @Builder.Default
+    @Pattern(regexp = "createdAt|updatedAt|totalPayment", message = "check documentation for sortBy specification")
+    String sortBy = "createdAt";
+
+    @Builder.Default
+    @Pattern(regexp = "DESC|ASC", message = "check documentation for sortDirection specification")
+    String sortDirection = "DESC";
+
+}
