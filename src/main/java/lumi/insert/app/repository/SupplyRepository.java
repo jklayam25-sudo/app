89e39f0a48fc578177d8ade9fe09ad3a4ea25e52
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-02 18:40:25 +0700

    feat(allSupply): implement supply relate base repository

---
+++ src/main/java/lumi/insert/app/repository/SupplyRepository.java
@@ -0,0 +1,17 @@
+package lumi.insert.app.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import lumi.insert.app.entity.Supply; 
+
+@Repository
+public interface SupplyRepository extends JpaRepository<Supply, UUID>, JpaSpecificationExecutor<Supply>{
+    
+    Optional<Supply> findByInvoiceId(String invoiceId);
+ 
+};
