d5369dd64ea9e37ebf3337dd52a5a8602a1e21e5
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-02 15:09:16 +0700

    sql(supplier): migrate create table suppliers

---
+++ src/main/resources/db/migration/V27__create_table_suppliers.sql
@@ -0,0 +1,13 @@
+CREATE TABLE suppliers (
+    id UUID PRIMARY KEY, 
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255),
+    contact VARCHAR(255) NOT NULL, 
+    is_active BOOLEAN DEFAULT TRUE NOT NULL, 
+    total_transaction BIGINT DEFAULT 0 NOT NULL,
+    total_unpaid BIGINT DEFAULT 0 NOT NULL,
+    total_paid BIGINT DEFAULT 0 NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    version INTEGER DEFAULT 0 NOT NULL
+);
