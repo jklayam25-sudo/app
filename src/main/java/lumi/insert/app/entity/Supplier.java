1bd9bc162b7857dc192b694bbf7b75862a20c35c
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-02 15:08:54 +0700

    feat(supplier): create supplier entity

---
+++ src/main/java/lumi/insert/app/entity/Supplier.java
@@ -0,0 +1,56 @@
+package lumi.insert.app.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lumi.insert.app.entity.nondatabase.TimestampAuditing;
+
+@Entity(name = "suppliers")
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Supplier extends TimestampAuditing{
+    
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false )
+    private String name;
+ 
+    private String email;
+
+    @Column(nullable = false )
+    private String contact;  
+
+    @Builder.Default 
+    private Boolean isActive = true;
+
+    @Builder.Default 
+    private Long totalTransaction = 0L;
+
+    @Builder.Default
+    private Long totalUnpaid = 0L;
+
+    @Builder.Default
+    private Long totalPaid = 0L;
+
+    // @OneToMany(mappedBy = "suppliers")
+    // @Builder.Default
+    // @ToString.Exclude
+    // private List<Transaction> supplies = new ArrayList<>();
+}
