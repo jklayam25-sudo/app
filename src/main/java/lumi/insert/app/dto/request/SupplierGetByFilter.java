f40dba5e876508e499a8e778d35a6fade343963e
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-02 15:10:04 +0700

    feat(supplier): add supplier request/response DTO

---
+++ src/main/java/lumi/insert/app/dto/request/SupplierGetByFilter.java
@@ -0,0 +1,61 @@
+package lumi.insert.app.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupplierGetByFilter extends PaginationRequest{
+
+    @Pattern(regexp = "^[a-zA-Z0-9.-_ ]{5,30}$", message = "name has to be 5-30 length")
+    String name;
+
+    @Email(message = "email doesn't meet the email format")
+    String email;
+
+    String contact;
+
+    Boolean isActive;
+
+    @Builder.Default
+    @Min(value = 0, message = "minTotalItems minimal value is 0")
+    Long minTotalTransaction = 0L;
+
+    @Builder.Default
+    @Min(value = 0, message = "maxTotalItems minimal value is 0")
+    Long maxTotalTransaction = 9999990L;
+
+    @Builder.Default
+    @Min(value = 0, message = "minGrandTotal minimal value is 0")
+    Long minTotalUnpaid = 0L;
+
+    @Builder.Default
+    @Min(value = 0, message = "maxGrandTotal minimal value is 0")
+    Long maxTotalUnpaid = 9999999999990L;
+
+    @Builder.Default
+    @Min(value = 0, message = "minTotalUnpaid minimal value is 0")
+    Long minTotalPaid = 0L;
+
+    @Builder.Default
+    @Min(value = 0, message = "maxTotalUnpaid minimal value is 0")
+    Long maxTotalPaid = 9999999999990L;
+
+    @Builder.Default
+    @Pattern(regexp = "createdAt|updatedAt|totalTransaction|totalUnpaid|totalPaid|name", message = "check documentation for sortBy specification")
+    String sortBy = "createdAt";
+
+    @Builder.Default
+    @Pattern(regexp = "DESC|ASC", message = "check documentation for sortDirection specification")
+    String sortDirection = "DESC";
+}
---
+++ src/main/java/lumi/insert/app/dto/response/SupplierNameResponse.java
@@ -0,0 +1,7 @@
+package lumi.insert.app.dto.response;
+
+import java.util.UUID;
+
+public record SupplierNameResponse(UUID id, String name) {
+    
+}
