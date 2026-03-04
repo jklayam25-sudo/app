0a917161c9b7872737122ab15b52d6042fc5c929
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-04 18:56:48 +0700

    feat(supply): implement custom query to select supply relative root > supplyItem > product

--- src/main/java/lumi/insert/app/repository/SupplyRepository.java
+++ src/main/java/lumi/insert/app/repository/SupplyRepository.java
@@ -5,6 +5,7 @@ import java.util.UUID;
  
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import lumi.insert.app.entity.Supply; 
@@ -13,5 +14,12 @@ import lumi.insert.app.entity.Supply;
 public interface SupplyRepository extends JpaRepository<Supply, UUID>, JpaSpecificationExecutor<Supply>{
     
     Optional<Supply> findByInvoiceId(String invoiceId);
+
+    @Query("SELECT DISTINCT s " + 
+        "FROM supplies s LEFT JOIN FETCH " +
+        "s.supplyItems si LEFT JOIN FETCH " +
+        "si.product " +
+        "ORDER BY s.createdAt ASC")
+    Optional<Supply> findByIdDetail(UUID id);
  
 };
