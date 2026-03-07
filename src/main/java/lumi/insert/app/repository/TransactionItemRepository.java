15e3cad873bd140dc1c18cdfa59b57fb1fcadaff
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-07 11:46:29 +0700

    fix(trxItem): fix return type, singular > plural

--- src/main/java/lumi/insert/app/repository/TransactionItemRepository.java
+++ src/main/java/lumi/insert/app/repository/TransactionItemRepository.java
@@ -1,6 +1,6 @@
 package lumi.insert.app.repository;
  
-import java.util.Optional;
+import java.util.List; 
 import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
@@ -13,7 +13,9 @@ import lumi.insert.app.entity.TransactionItem;
 @Repository
 public interface TransactionItemRepository extends JpaRepository<TransactionItem, UUID>{
     
-    Optional<TransactionItem> findByTransactionIdAndProductId(UUID transactionId, Long productId);
+    List<TransactionItem> findByTransactionIdAndProductId(UUID transactionId, Long productId);
+
+    Slice<TransactionItem> findByTransactionIdAndProductId(UUID transactionId, Long productId, Pageable pageable);
 
     Slice<TransactionItem> findAllByTransactionId(UUID transactionId, Pageable pageable);
 
