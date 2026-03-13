32fef53ca195908fffd047cfde4cee11dcd0cd08
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-14 05:04:46 +0700

    add openapi springdoc annotation to Controller

--- src/main/java/lumi/insert/app/controller/wrapper/WebResponse.java
+++ src/main/java/lumi/insert/app/controller/wrapper/WebResponse.java
@@ -1,9 +1,15 @@
 package lumi.insert.app.controller.wrapper;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record WebResponse<T>(T data, String errors) {
+public record WebResponse<T>(
+    @Schema(description = "The actual data payload")
+    T data, 
+    
+    @Schema(description = "Error message if any", nullable = true)
+    String errors) {
     public static <T> WebResponse<T> getWrapper(T data, String errors){
         return new WebResponse<T>(data, errors);
     }
