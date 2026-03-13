d3c4e45c5e02383b243adfa93f8db7437fa9e1f3
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-14 05:20:40 +0700

    implement transactionDetailResponse mapper

--- src/main/java/lumi/insert/app/utils/mapper/AllTransactionMapper.java
+++ src/main/java/lumi/insert/app/utils/mapper/AllTransactionMapper.java
@@ -7,8 +7,9 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
- 
-import lumi.insert.app.dto.response.TransactionItemDelete;
+
+import lumi.insert.app.dto.response.TransactionDetailResponse;
+import lumi.insert.app.dto.response.TransactionItemDelete; 
 import lumi.insert.app.dto.response.TransactionItemResponse;
 import lumi.insert.app.dto.response.TransactionPaymentResponse;
 import lumi.insert.app.dto.response.TransactionResponse;
@@ -16,7 +17,7 @@ import lumi.insert.app.entity.Transaction;
 import lumi.insert.app.entity.TransactionItem;
 import lumi.insert.app.entity.TransactionPayment;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = ProductMapper.class)
 public interface AllTransactionMapper {
     
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -26,10 +27,13 @@ public interface AllTransactionMapper {
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
     TransactionResponse createTransactionResponseDto(Transaction transaction);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    TransactionDetailResponse createTransactionDetailResponseDto(Transaction transaction);
+
     @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
     @Mapping(target = "productId", source = "product.id")
     TransactionItemResponse createTransactionItemResponseDto(TransactionItem transactionItem);
-
+  
     @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
     @Mapping(target = "deleted", constant = "true")
     TransactionItemDelete createTransactionItemDeleteResponseDto(TransactionItem transactionItem);
