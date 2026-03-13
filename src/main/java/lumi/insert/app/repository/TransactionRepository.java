a29f6fa303a29684bf7e550ed83ffb8b613dc37c
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-14 05:16:40 +0700

    feat(trx): implement query to get detailed transaction < with item and product

--- src/main/java/lumi/insert/app/repository/TransactionRepository.java
+++ src/main/java/lumi/insert/app/repository/TransactionRepository.java
@@ -5,13 +5,23 @@ import java.util.UUID;
  
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+ 
 import lumi.insert.app.entity.Transaction;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID>, JpaSpecificationExecutor<Transaction>{
     
     Optional<Transaction> findByInvoiceId(String invoiceId);
+
+    @Query("SELECT DISTINCT t " + 
+        "FROM transactions t LEFT JOIN FETCH " +
+        "t.transactionItems ti LEFT JOIN FETCH " +
+        "ti.product " +
+        "WHERE t.id = :id " +
+        "ORDER BY t.createdAt ASC")
+    Optional<Transaction> findByIdDetail(@Param("id") UUID id);
  
 };
