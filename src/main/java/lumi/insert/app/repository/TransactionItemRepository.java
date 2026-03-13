e55811223ea626870e1b57fe5b01f61f75bb21ca
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-14 05:16:07 +0700

    feat(product): implement query to get product/transaction statistics

--- src/main/java/lumi/insert/app/repository/TransactionItemRepository.java
+++ src/main/java/lumi/insert/app/repository/TransactionItemRepository.java
@@ -1,14 +1,20 @@
 package lumi.insert.app.repository;
  
+
+import java.time.LocalDateTime;
 import java.util.List; 
 import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
  
 import lumi.insert.app.entity.TransactionItem;
+import lumi.insert.app.repository.projection.ProductRefund;
+import lumi.insert.app.repository.projection.ProductSale;
 
 @Repository
 public interface TransactionItemRepository extends JpaRepository<TransactionItem, UUID>{
@@ -18,5 +24,21 @@ public interface TransactionItemRepository extends JpaRepository<TransactionItem
     Slice<TransactionItem> findByTransactionIdAndProductId(UUID transactionId, Long productId, Pageable pageable);
 
     Slice<TransactionItem> findAllByTransactionId(UUID transactionId, Pageable pageable);
+ 
+    @Query(value = "SELECT ti.productName as productName, SUM(ti.quantity) as totalSold FROM " +
+        "transaction_items ti WHERE " +
+        "ti.createdAt between :startDate AND :endDate " +
+        "GROUP BY ti.productName " +
+        "ORDER BY SUM(ti.quantity) DESC"
+    )
+    List<ProductSale> getProductTopSales(@Param("startDate")LocalDateTime startDate, @Param("endDate")LocalDateTime endDate);
 
+    @Query(value = "SELECT ti.productName as productName, SUM(ti.quantity) as totalRefunded FROM " +
+        "transaction_items ti WHERE " +
+        "ti.quantity < 0 AND " +
+        "ti.createdAt between :startDate AND :endDate " +
+        "GROUP BY ti.productName " +
+        "ORDER BY SUM(ti.quantity) ASC"
+    )
+    List<ProductRefund> getProductTopRefund(@Param("startDate")LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 };
