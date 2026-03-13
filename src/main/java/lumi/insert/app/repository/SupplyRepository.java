17c183c7d274e5e7a1446b74da9c072db69c4095
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-14 05:14:50 +0700

    fix(supply): fix undetect param id

--- src/main/java/lumi/insert/app/repository/SupplyRepository.java
+++ src/main/java/lumi/insert/app/repository/SupplyRepository.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import lumi.insert.app.entity.Supply; 
@@ -19,7 +20,8 @@ public interface SupplyRepository extends JpaRepository<Supply, UUID>, JpaSpecif
         "FROM supplies s LEFT JOIN FETCH " +
         "s.supplyItems si LEFT JOIN FETCH " +
         "si.product " +
+        "WHERE s.id = :id " +
         "ORDER BY s.createdAt ASC")
-    Optional<Supply> findByIdDetail(UUID id);
+    Optional<Supply> findByIdDetail(@Param("id") UUID id);
  
 };
