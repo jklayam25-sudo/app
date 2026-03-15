6e5cf88b32a7cdd4225a5a9392f07ea2274a1393
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-16 05:10:39 +0700

    feat(xlsx): add styling and sum total

--- src/main/java/lumi/insert/app/service/implement/XlsxServiceImpl.java
+++ src/main/java/lumi/insert/app/service/implement/XlsxServiceImpl.java
@@ -1,9 +1,13 @@
 package lumi.insert.app.service.implement;
  
 import java.io.OutputStream;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.List;
 
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.CellStyle; 
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.util.CellRangeAddress;
@@ -11,6 +15,7 @@ import org.apache.poi.xssf.streaming.SXSSFCell;
 import org.apache.poi.xssf.streaming.SXSSFRow;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 import lumi.insert.app.dto.response.SupplyResponse;
@@ -18,7 +23,11 @@ import lumi.insert.app.dto.response.TransactionResponse;
 import lumi.insert.app.service.XlsxService;
 
 @Slf4j
+@Service
 public class XlsxServiceImpl implements XlsxService{
+
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG, FormatStyle.SHORT);
+
 
     @Override
     public void exportTransactions(List<TransactionResponse> datas, OutputStream outputStream) {
@@ -28,10 +37,18 @@ public class XlsxServiceImpl implements XlsxService{
             SXSSFCell titleCell = titleRow.createCell(0);
             titleCell.setCellValue("Transaction List: ");
             sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 14)); 
-            
+
+            short px12 = 12;
+            Font font = workbook.createFont();
+            font.setBold(true);
+
+            font.setFontHeightInPoints(px12);
+
             CellStyle defaultCenter = workbook.createCellStyle();
             defaultCenter.setAlignment(HorizontalAlignment.CENTER); 
             defaultCenter.setVerticalAlignment(VerticalAlignment.CENTER); 
+            defaultCenter.setFillBackgroundColor(HSSFColor.HSSFColorPredefined.LIGHT_BLUE.getIndex());
+            defaultCenter.setFont(font);
             titleCell.setCellStyle(defaultCenter);
 
             SXSSFRow headerRow = sheet.createRow(1);
@@ -50,12 +67,28 @@ public class XlsxServiceImpl implements XlsxService{
             headerRow.createCell(12).setCellValue("Status");
             headerRow.createCell(13).setCellValue("Transaction Date");
             headerRow.createCell(14).setCellValue("Last Update"); 
+            
+            CellStyle currencyStyle = workbook.createCellStyle();
+            currencyStyle.setDataFormat(workbook.createDataFormat().getFormat("[$Rp-421]#,##0.00;[RED]-[$Rp-421]#,##0.00"));    
 
+            for (int x = 1; x < 15; x++) {
+                if(x > 11){
+                    sheet.setColumnWidth(x,  24 * 256);
+                } else {
+                    sheet.setColumnWidth(x,  20 * 256); 
+                    sheet.setDefaultColumnStyle(x, currencyStyle); 
+                }
+            }
+            headerRow.setRowStyle(defaultCenter);
+
+            sheet.setColumnWidth(1,  24 * 256);
+            sheet.setColumnWidth(2,  28 * 256);
+ 
             int i = 2;
             for (TransactionResponse data : datas) {
                 SXSSFRow row = sheet.createRow(i);  
 
-                row.createCell(0).setCellValue(i);
+                row.createCell(0).setCellValue(i-1);
                 row.createCell(1).setCellValue(data.invoiceId());
                 row.createCell(2).setCellValue(data.customerName());
                 row.createCell(3).setCellValue(data.totalItems());
@@ -68,16 +101,25 @@ public class XlsxServiceImpl implements XlsxService{
                 row.createCell(10).setCellValue(data.totalUnrefunded());
                 row.createCell(11).setCellValue(data.totalRefunded());
                 row.createCell(12).setCellValue(String.valueOf(data.status()));
-                row.createCell(13).setCellValue(data.createdAt());
-                row.createCell(14).setCellValue(data.updatedAt()); 
+                row.createCell(13).setCellValue(data.createdAt().format(dateTimeFormatter));
+                row.createCell(14).setCellValue(data.updatedAt().format(dateTimeFormatter)); 
 
                 i++;
-            }
-            CellStyle currencyStyle = workbook.createCellStyle();
-            currencyStyle.setDataFormat(workbook.createDataFormat().getFormat("Rp#,##0"));  
-            sheet.setDefaultColumnStyle(7, currencyStyle);
+            } 
 
-            sheet.createFreezePane(0, 1);
+            SXSSFRow totalRow = sheet.createRow(i); 
+            totalRow.createCell(2).setCellValue("SUM: "); 
+            totalRow.createCell(3).setCellFormulaImpl("=SUM(D3:D" + i); 
+            totalRow.createCell(4).setCellFormulaImpl("=SUM(E3:E" + i); 
+            totalRow.createCell(5).setCellFormulaImpl("=SUM(F3:F" + i); 
+            totalRow.createCell(6).setCellFormulaImpl("=SUM(G3:G" + i); 
+            totalRow.createCell(7).setCellFormulaImpl("=SUM(H3:H" + i); 
+            totalRow.createCell(8).setCellFormulaImpl("=SUM(I3:I" + i); 
+            totalRow.createCell(9).setCellFormulaImpl("=SUM(J3:J" + i); 
+            totalRow.createCell(10).setCellFormulaImpl("=SUM(K3:K" + i); 
+            totalRow.createCell(11).setCellFormulaImpl("=SUM(L3:L" + i); 
+
+            sheet.createFreezePane(0, 2);
             workbook.write(outputStream);
         } catch (Exception e) {
            log.warn("Exception triggered during exporting stream to client, with message: {}", e.getLocalizedMessage());
@@ -93,9 +135,17 @@ public class XlsxServiceImpl implements XlsxService{
             titleCell.setCellValue("Supply List: ");
             sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 14)); 
             
+            short px12 = 12;
+            Font font = workbook.createFont();
+            font.setBold(true);
+            
+            font.setFontHeightInPoints(px12);
+
             CellStyle defaultCenter = workbook.createCellStyle();
             defaultCenter.setAlignment(HorizontalAlignment.CENTER); 
-            defaultCenter.setVerticalAlignment(VerticalAlignment.CENTER); 
+            defaultCenter.setVerticalAlignment(VerticalAlignment.CENTER);
+            defaultCenter.setFillBackgroundColor(HSSFColor.HSSFColorPredefined.GOLD.getIndex());
+            defaultCenter.setFont(font); 
             titleCell.setCellStyle(defaultCenter);
 
             SXSSFRow headerRow = sheet.createRow(1);
@@ -114,11 +164,24 @@ public class XlsxServiceImpl implements XlsxService{
             headerRow.createCell(12).setCellValue("Status");
             headerRow.createCell(13).setCellValue("Transaction Date"); 
 
+            CellStyle currencyStyle = workbook.createCellStyle();
+            currencyStyle.setDataFormat(workbook.createDataFormat().getFormat("[$Rp-421]#,##0.00;[RED]-[$Rp-421]#,##0.00"));   
+            for (int x = 1; x < 15; x++) {
+                if(x > 11){
+                    sheet.setColumnWidth(x,  24 * 256);
+                } else {
+                    sheet.setColumnWidth(x,  20 * 256); 
+                    sheet.setDefaultColumnStyle(x, currencyStyle); 
+                }
+            }
+
+            headerRow.setRowStyle(defaultCenter);
+
             int i = 2;
             for (SupplyResponse data : datas) {
                 SXSSFRow row = sheet.createRow(i);  
 
-                row.createCell(0).setCellValue(i);
+                row.createCell(0).setCellValue(i-1);
                 row.createCell(1).setCellValue(data.invoiceId());
                 row.createCell(2).setCellValue(data.supplierName());
                 row.createCell(3).setCellValue(data.totalItems());
@@ -131,15 +194,25 @@ public class XlsxServiceImpl implements XlsxService{
                 row.createCell(10).setCellValue(data.totalUnrefunded());
                 row.createCell(11).setCellValue(data.totalRefunded());
                 row.createCell(12).setCellValue(String.valueOf(data.status()));
-                row.createCell(13).setCellValue(data.createdAt()); 
+                row.createCell(13).setCellValue(data.createdAt().format(dateTimeFormatter)); 
 
                 i++;
             }
-            CellStyle currencyStyle = workbook.createCellStyle();
-            currencyStyle.setDataFormat(workbook.createDataFormat().getFormat("Rp#,##0"));  
-            sheet.setDefaultColumnStyle(7, currencyStyle);
 
-            sheet.createFreezePane(0, 1);
+            SXSSFRow totalRow = sheet.createRow(i); 
+            totalRow.createCell(2).setCellValue("SUM: "); 
+            totalRow.createCell(3).setCellFormulaImpl("=SUM(D3:D" + i); 
+            totalRow.createCell(4).setCellFormulaImpl("=SUM(E3:E" + i); 
+            totalRow.createCell(5).setCellFormulaImpl("=SUM(F3:F" + i); 
+            totalRow.createCell(6).setCellFormulaImpl("=SUM(G3:G" + i); 
+            totalRow.createCell(7).setCellFormulaImpl("=SUM(H3:H" + i); 
+            totalRow.createCell(8).setCellFormulaImpl("=SUM(I3:I" + i); 
+            totalRow.createCell(9).setCellFormulaImpl("=SUM(J3:J" + i); 
+            totalRow.createCell(10).setCellFormulaImpl("=SUM(K3:K" + i); 
+            totalRow.createCell(11).setCellFormulaImpl("=SUM(L3:L" + i); 
+
+            
+            sheet.createFreezePane(0, 2);
             workbook.write(outputStream);
         } catch (Exception e) {
             log.warn("Exception triggered during exporting stream to client, with message: {}", e.getLocalizedMessage());
